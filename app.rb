5cad7cf5511ab8deece09c049e4bbd48529c9d32
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-23 22:14:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ class App < Sinatra::Base
     @coach = params[:coach]
     @point_guard = params[:pg]
     @shooting_guard = params[:sg]
+    @small_forward = params[:sf]
     @power_forward = params[:pf]
     @center = params[:c]
   
