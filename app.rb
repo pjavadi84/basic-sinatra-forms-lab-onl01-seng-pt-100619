6bc961fdeb38ba22811a965343ce4ffee617c3de
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-23 22:11:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    @team_name = params[:team]
+    @team_name = params[:team_name]
     @coach = params[:coach]
     @point_guard = params[:pg]
     @shooting_guard = params[:sg]
